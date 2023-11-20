646f66d0f762bb8fdc5566c5ebd2fc5d2f4cbcd8
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-11-20 22:44:17 +0400

    add module Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from "./pages/components/AppRouter";
 import { Header } from "./modules/Header";
+import { Footer } from './modules/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="app_container w-100 h-100">
         <Header />
         <AppRouter />
+        <Footer/>
       </div>
     </BrowserRouter>
   );
